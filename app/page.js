802ef4e416ce27b07a2ed8b3ae4ffa98901dee9e
Rasmus Lindahl <rasmus.lindahl1996@gmail.com>
2024-10-15 09:11:32 +0200

    Set default to top last three days

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import PaperDashboard from './PaperDashboard';
 import { getPapers } from './utils';
 
 export default async function Home() {
-  const initialTimeFrame = 'today'; // Default value for server-side rendering
+  const initialTimeFrame = 'three_days'; // Default value for server-side rendering
   const initialPapers = await getPapers(initialTimeFrame);
   return <PaperDashboard initialPapers={initialPapers} initialTimeFrame={initialTimeFrame} />;
 }
